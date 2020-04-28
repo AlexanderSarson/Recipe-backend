b697bad46eac1b3de6a6088a4ac2679bd0bd7ed4
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2020-04-28 08:27:09 +0200

    Remove the whole class

--- src/test/java/spoonacular/FoodFacadeTest.java
+++ src/test/java/spoonacular/FoodFacadeTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@Disabled
 public class FoodFacadeTest {
-    private FoodFacade foodFacade = new FoodFacade();
+    /*private FoodFacade foodFacade = new FoodFacade();
 
     @Disabled
     @Test
@@ -20,5 +20,5 @@ public class FoodFacadeTest {
 
         assertEquals("Falafel Burgers with Feta Cucumber Sauce", result.getTitle());
         assertEquals(492564,result.getId());
-    }
+    }*/
 }
