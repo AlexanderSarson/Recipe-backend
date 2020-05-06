23f3030c0b4fed009af862d5564e82f4af42afda
Benjamin Paepke <cph-bp133@cphbusiness.dk>
2020-05-06 14:25:28 +0200

    Merge master into branch + removed code smell

--- src/main/java/rest/IngredientResource.java
+++ src/main/java/rest/IngredientResource.java
@@ -1,6 +1,5 @@
 package rest;
 
-import dtos.FoodIngredientDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
